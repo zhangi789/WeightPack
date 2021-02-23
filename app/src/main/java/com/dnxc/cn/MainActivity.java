a8d7e0a27681286fcf7352bd60442d56d90851e4
zhang@68353 <www.392032143@qq.com>
2021-02-23 10:26:35 +0800

    圆3指示器

--- app/src/main/java/com/dnxc/cn/MainActivity.java
+++ app/src/main/java/com/dnxc/cn/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.zhy.widget.CircleIndicatorView;
+
 public class MainActivity extends AppCompatActivity {
 
+    CircleIndicatorView mCircleIndicatorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCircleIndicatorView=findViewById(R.id.mm);
+        mCircleIndicatorView.setTotalSize(4);
+        mCircleIndicatorView.setCurrentPostion(1);
     }
 }
